139fc9cfac3ae4ee6192346d8032c94f3a0134d7
Samuel <samuelpierrej@gmail.com>
2026-01-20 10:33:39 -0300

    feat: new database model

--- api/designations/src/extract_data/models.py
+++ api/designations/src/extract_data/models.py
@@ -1,1 +1,24 @@
 from sqlmodel import SQLModel, Field
+from typing import List, Dict, Optional
+
+class Meetings(SQLModel, table=True):
+    __tablename__ = "meetings"
+
+    id: Optional[int] = Field(primary_key=True)
+
+    week: str = Field(...)
+    bible_chapter: str = Field(...)
+    music_and_introduction_words: str = Field(...)
+
+    first_title: str = Field(...)
+    first_part_title: str = Field(...)
+    second_part_title: str = Field(...)
+    bible_lecture: str = Field(...)
+
+    second_title: str = Field(...)
+    parts_of_second_title: List[Dict]
+
+    third_title: str = Field(...)
+    parts_of_third_title: List[Dict]
+
+    conclusion_words: str = Field(...)
